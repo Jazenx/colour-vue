53f47b2ea4ef255c91bf8771d30f2043f0faf64d
Jason <jason.zhour@gmail.com>
2017-08-21 11:56:11 +0800

    create layout menu

---
+++ src/views/layout/layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-wrapper">
+    <sidebar class="sidebar-container"></sidebar>
+    <div class="main-container">
+      <navbar></navbar>
+      <app-main></app-main>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+    
+}
+</script>
